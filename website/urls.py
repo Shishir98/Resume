2c6643b8c32d8116121ee6f21705fb80526cfabc
Shishir <shishir98@live.com>
2023-11-20 22:05:29 +0530

    Updated to latest changes

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = "index" ),
-
+    path('download/resume',views.download_resume, name="download"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,23 @@
+import os
+from django.http import FileResponse
 from django.shortcuts import render
 
-# Create your views here.
+
 def index(request):
     return render(request, 'index.html', {})
+
+
+def download_resume(request):
+    # Check if the uploaded file is a PDF
+    # Use os.path.join for creating file paths
+    pdf_file_path = os.path.join('static', 'website', 'documents', 'Shishir Saxena - Resume.docx.pdf')
+
+    # Provide a custom name for the downloaded file (optional)
+    custom_file_name = 'resume.pdf'
+
+    # Make sure the file exists before attempting to serve it
+    if os.path.exists(pdf_file_path):
+        return FileResponse(open(pdf_file_path, 'rb'), as_attachment=True, filename=custom_file_name)
+    else:
+        # Handle the case where the file doesn't exist
+        return render(request, 'file_not_found.html', {})
